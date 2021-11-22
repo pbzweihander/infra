d9c50e55ee9ff463dea7283a9b9240257fa844ac
pbzweihander <pbzweihander@gmail.com>
2021-11-22 22:47:20 +0900

    Update terraform

--- base.tf
+++ base.tf
@@ -1,16 +1,22 @@
 provider "aws" {
-  version = "~> 2.23"
-  region  = "ap-northeast-1"
+  region = "ap-northeast-1"
 }
 
 terraform {
-  required_version = "~> 0.12.6"
+  required_version = "~> 1.0.8"
 
   backend "remote" {
     organization = "pbzweihander"
 
     workspaces {
       name = "infra"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.66.0"
     }
   }
 }
