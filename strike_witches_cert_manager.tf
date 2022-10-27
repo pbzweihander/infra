d32973741ab7471ea3bd0d52b20154f1cfec6805
pbzweihander <pbzweihander@gmail.com>
2022-10-27 22:08:48 +0900

    Add cert-manager for strike-witches cluster

---
+++ strike_witches_cert_manager.tf
@@ -0,0 +1,14 @@
+resource "helm_release" "strike_witches_cert_manager" {
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "1.10.0"
+
+  name             = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
