0100a5c1a4ddafc08c63b2eb4c11c76aa9e8b6b1
pbzweihander <pbzweihander@gmail.com>
2022-10-28 15:48:58 +0900

    Add rust-trending

---
+++ rust_trending.tf
@@ -0,0 +1,116 @@
+locals {
+  rust_trending_bot_selector_labels = {
+    "app.kubernetes.io/name"      = "rust-trending"
+    "app.kubernetes.io/instance"  = "rust-trending"
+    "app.kubernetes.io/component" = "bot"
+  }
+  rust_trending_redis_selector_labels = {
+    "app.kubernetes.io/name"      = "rust-trending"
+    "app.kubernetes.io/instance"  = "rust-trending"
+    "app.kubernetes.io/component" = "redis"
+  }
+}
+
+resource "kubernetes_namespace" "rust_trending" {
+  provider = kubernetes.strike_witches
+
+  metadata {
+    name = "rust-trending"
+  }
+}
+
+resource "kubernetes_deployment" "rust_trending_bot" {
+  provider = kubernetes.strike_witches
+
+  metadata {
+    namespace = kubernetes_namespace.rust_trending.metadata[0].name
+    name      = "rust-trending-bot"
+    labels    = local.rust_trending_bot_selector_labels
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = local.rust_trending_bot_selector_labels
+    }
+    template {
+      metadata {
+        labels = local.rust_trending_bot_selector_labels
+      }
+      spec {
+        container {
+          name    = "bot"
+          image   = "ghcr.io/pbzweihander/rust-trending:7bd212e60af0182198c79cf004a4f0e4136ebdfb"
+          command = ["rust-trending", "/config/config.toml"]
+          env {
+            name  = "RUST_LOG"
+            value = "info"
+          }
+          env {
+            name  = "RUST_BACKTRACE"
+            value = "1"
+          }
+          volume_mount {
+            name       = "config"
+            mount_path = "/config"
+            read_only  = true
+          }
+        }
+        volume {
+          name = "config"
+          secret {
+            secret_name = "rust-trending"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment" "rust_trending_redis" {
+  provider = kubernetes.strike_witches
+
+  metadata {
+    namespace = kubernetes_namespace.rust_trending.metadata[0].name
+    name      = "rust-trending-redis"
+    labels    = local.rust_trending_redis_selector_labels
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = local.rust_trending_redis_selector_labels
+    }
+    template {
+      metadata {
+        labels = local.rust_trending_redis_selector_labels
+      }
+      spec {
+        container {
+          name  = "redis"
+          image = "redis:4-alpine"
+          port {
+            container_port = 6379
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "rust_trending_redis" {
+  provider = kubernetes.strike_witches
+
+  metadata {
+    namespace = kubernetes_namespace.rust_trending.metadata[0].name
+    name      = "rust-trending-redis"
+    labels    = local.rust_trending_redis_selector_labels
+  }
+  spec {
+    type     = "ClusterIP"
+    selector = local.rust_trending_redis_selector_labels
+    port {
+      port        = 6379
+      protocol    = "TCP"
+      target_port = 6379
+    }
+  }
+}
