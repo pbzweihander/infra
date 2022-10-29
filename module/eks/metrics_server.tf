d33b120e0100ea17fd4762da42cfb22daa04bade
pbzweihander <pbzweihander@gmail.com>
2022-10-29 16:24:05 +0900

    Add metrics-server for strike-witches cluster

---
+++ module/eks/metrics_server.tf
@@ -0,0 +1,9 @@
+resource "helm_release" "metrics_server" {
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  version    = "3.8.2"
+
+  name             = "metrics-server"
+  namespace        = "metrics-server"
+  create_namespace = true
+}
