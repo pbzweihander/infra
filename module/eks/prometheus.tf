536da5c08dcddeaeb59ca41691fd264dd4924e7f
pbzweihander <pbzweihander@gmail.com>
2023-07-09 04:27:03 +0900

    Remove openobserve

--- module/eks/prometheus.tf
+++ module/eks/prometheus.tf
@@ -5,16 +5,4 @@ resource "helm_release" "prometheus" {
   name             = "prometheus"
   namespace        = "prometheus"
   create_namespace = true
-
-  values = [yamlencode({
-    server = {
-      remoteWrite = [{
-        url = "http://openobserve-router.${local.openobserve_namespace}.svc.cluster.local:5080/api/default/prometheus/api/v1/write"
-        basic_auth = {
-          username = "pbzweihander@gmail.com"
-          password = random_password.openobserve.result
-        }
-      }]
-    }
-  })]
 }
